3a427cc923ab64f06631d7b308189e548bd8a01b
robyng <robyngraham01@gmail.com>
2022-01-12 11:24:13 -0800

    use readme-template file and export to index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const init = require('./src/readme-template.js');
 
 const questions = () => {
     
@@ -126,65 +127,3 @@ questions()
 
 
 })
-
-
-const init = (answers) => {
-   let licenseText = ''
-   switch (answers.license) {
-       case 'MIT':
-       licenseText = 'The MIT License is a permissive free software license originating at the Massachusetts Institute of Technology in the late 1980s. As a permissive license, it puts only very limited restriction on reuse and has, therefore, high license compatibility.';
-       break;
-       case 'APACHE 2.0':
-       licenseText = 'The 2.0 version of the Apache License, approved by the ASF in 2004, helps us achieve our goal of providing reliable and long-lived software products through collaborative, open-source software development.';
-       break;
-       case 'GPL 3.0':
-       licenseText = 'The GNU General Public License is a free, copyleft license for software and other kinds of works.';
-       break;
-       case 'BSD 3':
-       licenseText = 'The BSD 3-clause license allows you almost unlimited freedom with the software so long as you include the BSD copyright and license';
-       break;
-       default: licenseText = 'Copyright Protected'
-   }
-return `# ${answers.projectName} 
-![Packagist License](https://img.shields.io/static/v1?label=License&message=${answers.license}&color=blue)
-
-
-## Table of Contents
-* [About](#about)
-* [Links](#links)
-* [Installation](#installation)
-* [Credits](#credits)
-* [License](#license)
-* [Questions](#questions)
-
-## About 
-${answers.description}
-
-${answers.usage}
-
-### Links
-
-Deployed at ${answers.deployed}
-
-Repository at ${answers.repo}
-
-### Installation
-${answers.install}
-
-### Credits
-${answers.contributers}
-
-### License
-${answers.license}
-${licenseText}
-
-
-### Questions
-Github profile: https://github.com/${answers.github}
-
-Have any questions?
-
-Please email us at ${answers.email}
-
-${answers.issues}`;    
-}
--- output/README.md
+++ output/README.md
@@ -1,42 +1,42 @@
-# Readme Generator 
-![Packagist License](https://img.shields.io/static/v1?label=License&message=MIT&color=blue)
-
-
-## Table of Contents
-* [About](#about)
-* [Links](#links)
-* [Installation](#installation)
-* [Credits](#credits)
-* [License](#license)
-* [Questions](#questions)
-
-## About 
-This generates a readme for you.
-
-Answer the questions
-
-### Links
-
-Deployed at test
-
-Repository at test
-
-### Installation
-npm install, then npm index
-
-### Credits
-Robyn
-
-### License
-MIT
-The MIT License is a permissive free software license originating at the Massachusetts Institute of Technology in the late 1980s. As a permissive license, it puts only very limited restriction on reuse and has, therefore, high license compatibility.
-
-
-### Questions
-Github profile: https://github.com/robyng
-
-Have any questions?
-
-Please email us at robyn@sitereworks.com
-
-contact me at robyn@sitereworks.com
+# test 
+ ![Packagist License](https://img.shields.io/static/v1?label=License&message=BSD 3&color=blue)
+ 
+ 
+ ## Table of Contents
+ * [About](#about)
+ * [Links](#links)
+ * [Installation](#installation)
+ * [Credits](#credits)
+ * [License](#license)
+ * [Questions](#questions)
+ 
+ ## About 
+ d
+ 
+ d
+ 
+ ### Links
+ 
+ Deployed at d
+ 
+ Repository at d
+ 
+ ### Installation
+ d
+ 
+ ### Credits
+ d
+ 
+ ### License
+ BSD 3
+ The BSD 3-clause license allows you almost unlimited freedom with the software so long as you include the BSD copyright and license
+ 
+ 
+ ### Questions
+ Github profile: https://github.com/test
+ 
+ Have any questions?
+ 
+ Please email us at d
+ 
+ d
